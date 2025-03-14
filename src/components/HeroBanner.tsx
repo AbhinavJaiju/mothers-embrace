f3afb54abdfc9a564611a14950e4bcfda63fb51b
AbhinavJaiju <abhinavchalakudi@gmail.com>
2025-03-14 15:13:15 +0530

    color changes

--- src/components/HeroBanner.tsx
+++ src/components/HeroBanner.tsx
@@ -10,7 +10,10 @@ export default function HeroBanner() {
         <div className="relative flex h-full items-center justify-center bg-gray-400 dark:bg-gray-700 dark:text-white">
           <img src={Baby1} alt="Baby1" className="w-full h-full object-cover" />
           <div className="absolute bottom-8 left-0 w-full text-center text-white py-2">
-            Slide 1
+          <div className="absolute inset-0 mx-4 my-auto flex h-64 flex-col items-center justify-center rounded-lg text-red-500 sm:text-gray-100 bg-white/50 px-4 pb-6 text-center sm:mx-6 sm:my-4 sm:h-full sm:bg-transparent">
+            <h2 className="text-4xl font-bold">Nurturing Life from the Start!</h2>
+            <span>Regular check-ups, proper nutrition, and guided exercises ensure a healthy pregnancy for both mother and baby</span>
+            </div>
           </div>
         </div>
         {/* <div className="relative flex h-full items-center justify-center bg-gray-400 dark:bg-gray-700 dark:text-white">
@@ -22,7 +25,10 @@ export default function HeroBanner() {
         <div className="relative flex h-full items-center justify-center bg-gray-400 dark:bg-gray-700 dark:text-white">
           <img src={Baby3} alt="Baby3" className="w-full h-full object-cover" />
           <div className="absolute bottom-8 left-0 w-full text-center text-white py-2">
-            Slide 3
+          <div className="absolute inset-0 mx-4 my-auto flex h-64 flex-col items-center justify-center text-yellow-300 sm:text-gray-100 rounded-lg bg-white/50 px-4 pb-6 text-center sm:mx-6 sm:my-4 sm:h-full sm:bg-transparent">
+          <h2 className="text-4xl font-bold">A Smooth Journey into Motherhood!</h2>
+          <span>Postpartum recovery, lactation support, and mental well-being assistance help mothers regain strength and confidence.</span>
+          </div>  
           </div>
         </div>
       </Carousel>
