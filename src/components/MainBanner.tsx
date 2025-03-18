ee4082e262c37ea41dc20f9aac5ebd6270291e96
AbhinavJaiju <abhinavchalakudi@gmail.com>
2025-03-18 20:48:37 +0530

    services

--- src/components/MainBanner.tsx
+++ src/components/MainBanner.tsx
@@ -24,7 +24,7 @@ const MainBanner = () => {
         className="h-90 w-full object-cover blur-sm filter sm:h-90 md:h-90 lg:h-90 xl:h-90"
       />
       <div className="absolute inset-0 mx-4 my-auto flex h-96 flex-col items-center justify-center rounded-lg bg-white/50 px-4 pb-6 text-center sm:mx-6 sm:my-4 sm:h-full sm:bg-transparent">
-        <span className=" text-6xl font-bold text-blue-500 sm:text-8xl sm:text-gray-300">
+        <span className=" text-6xl font-bold text-blue-500 sm:text-8xl sm:text-gray-400">
           Mother's Embrace
         </span>
         <span className="mt-2 py-6 text-3xl text-pink-500 sm:py-4 sm:text-6xl sm:text-red-400">
